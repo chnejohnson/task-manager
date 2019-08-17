e7336d033cfbdbd1adcc3c12e8fd64e120792593
chnejohnson <chnejohnson@gmail.com>
2019-08-17 20:58:19 +0800

    add cors origin

--- dist/middleware/cors.js
+++ dist/middleware/cors.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const url = process.env.CORS_DOMAIN;
 function default_1(req, res, next) {
     res.header("Access-Control-Allow-Origin", url);
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization");
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
     next();
 }
--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -4,7 +4,10 @@ const url: any = process.env.CORS_DOMAIN;
 
 export default function(req: Request, res: Response, next: NextFunction) {
   res.header("Access-Control-Allow-Origin", url);
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, Authorization"
+  );
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
 }
