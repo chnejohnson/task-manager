b31c03fbd7c407ca86aa34728394a1837d506c48
chnejohnson <chnejohnson@gmail.com>
2019-08-17 20:55:04 +0800

    add cors methods

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -5,5 +5,6 @@ const url: any = process.env.CORS_DOMAIN;
 export default function(req: Request, res: Response, next: NextFunction) {
   res.header("Access-Control-Allow-Origin", url);
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
 }
