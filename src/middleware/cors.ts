1b1b0671299c3952c29601b0d394229b1bdb7688
chnejohnson <chnejohnson@gmail.com>
2019-08-17 21:56:36 +0800

    restore cors

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -4,10 +4,7 @@ const url: any = process.env.CORS_DOMAIN;
 
 export default function(req: Request, res: Response, next: NextFunction) {
   res.header("Access-Control-Allow-Origin", url);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, Content-Type, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   next();
 }
